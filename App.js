f4f526c4f454f28d3b440f4ec9836130be84c910
rosariodev <48676177+rosariodev@users.noreply.github.com>
2020-09-08 18:56:05 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Hearder from './Hearder'
+import Main from './Main.js'
+import Footer from './Footer.js'
+
+export default class App extends React.Component{
+
+  
+
+
+    render(){
+        return(
+            <div>
+                <Hearder/>
+                <Main/>
+                <Footer/>
+                
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <div id="home"> home</div> 
+                  <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/> <br/>
+            </div>
+        )
+    }
+
+} 
---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/index.css'
+import App from '../componentes/App'
+import Menoo from '../componentes/Menoo'
+
+
+
+
+ReactDOM.render(
+    <Menoo/>, document.getElementById('todowrapper')
+
+)
